5ff9919ee45055494c5b10a864abb706e5c91e71
Semensera <cvdfjdfg@gmail.com>
2024-11-05 23:38:14 +0200

    Add files via upload

---
+++ array.js
@@ -0,0 +1,8 @@
+const carname = "BMW X5"; 
+const caryear = 2020; 
+let carprice = 1500030;
+let available= true;
+available = 'в наявності'
+let buyername = null;
+buyername = 'немає'
+console.log(`Продаю машину. Назва: ${carname}, Рік: ${caryear}, Ціна ${carprice} гривень, Наявність: ${available}, Покупця: ${buyername}`)
